385e56e00a153cedf3829fb82e6f58991b8615f9
Aleksandr Batsuev <batsuev@gmail.com>
2014-07-14 15:28:06 +0900

    resize + finalize local migration

--- resources/public/js/docs.js
+++ resources/public/js/docs.js
@@ -10,4 +10,23 @@ $(function() {
 	}
 	return false;
     });
+
+    var resize = function(e) {
+	var w = e.pageX;
+	$("#sidebar").css("width", w - 5);
+	$("#content").css("left", w);
+	$(".resizer").css("left", w);
+    };
+
+    var stopResizing = function() {
+	$("#locker").hide();
+	$(window).off("mousemove", resize);
+	$(window).off("mouseup", stopResizing);
+    };
+
+    $(".resizer").mousedown(function() {
+	$("#locker").show();
+	$(window).on("mousemove", resize);
+	$(window).on("mouseup", stopResizing);
+    });
 });
