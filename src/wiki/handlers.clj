ea48b60eb539cad223dcebde53edb925540bfc90
Alex Batsuev <batsuev@gmail.com>
2014-10-23 16:21:32 +0900

    DVF-1014 fixes

--- src/wiki/handlers.clj
+++ src/wiki/handlers.clj
@@ -8,15 +8,24 @@
             [wiki.documents :as docs]
             [ring.util.response :refer [response]]
             [ring.middleware.json :refer [wrap-json-response]]
+            [org.httpkit.client :as http]
+            [cheshire.core :refer [generate-string]]
             [wiki.md :as md])
   (:gen-class :main :true))
 
 (defn get-env-from-domain [request]
   (:server-name request))
 
+(defn notify-slack []
+  (http/post "https://anychart-team.slack.com/services/hooks/incoming-webhook?token=P8Z59E0kpaOqTcOxner4P5jb"
+             {:form-params {:payload (generate-string {:text (str "Documentation deployed")
+                                                       :channel "#notifications"
+                                                       :username "docs-engine"})}}))
+
 (defn rebuild [request]
   (versions/update)
-  "Ты пришел и говоришь: движок, мне нужна документация. Но ты просишь без уважения, ты не предлагаешь дружбу, ты даже не сделал мне pull request.<br />Тем не менее я выполню твою просьбу.")
+  (notify-slack)
+  (str "done"))
 
 (defn redirect-version [request]
   (redirect (str "/" (-> request :route-params :version) "/Quick_Start")))
@@ -29,7 +38,7 @@
         (app request version doc)
         (route/not-found "Document not found")))))
 
-(defn show-document [request version doc]
+(defn do-show-document [request version doc]
   (let [md-path (docs/md-path version doc)] 
     (render-file "templates/page.html" {:versions (versions/versions)
                                         :version version
@@ -41,6 +50,17 @@
                                                   version
                                                   (docs/get-content md-path)
                                                   (get-env-from-domain request))})))
+
+(defn show-document [request]
+  (let [version (-> request :route-params :version)
+        path (-> request :route-params :*)]
+    (if (docs/exists? version path)
+      (do-show-document request version path)
+      (let [group (clojure.string/replace path #"\/$" "")
+            doc (docs/get-group-first-doc version group)]
+        (if doc
+          (redirect (str "/" version "/" doc))
+          (route/not-found "Document not found"))))))
 
 (defn show-document-json [request version doc]
   (let [md-path (docs/md-path version doc)]
@@ -59,7 +79,7 @@
   (GET "/:version" [version] redirect-version)
   (GET "/:version/" [version] redirect-version)
   (GET "/:version/*-json" [version doc] (check-document-middleware show-document-json))
-  (GET "/:version/*" [version doc] (check-document-middleware show-document))
+  (GET "/:version/*" [version doc] show-document)
   (route/not-found "Page not found"))
 
 (def app
--- src/wiki/md.clj
+++ src/wiki/md.clj
@@ -4,10 +4,20 @@
             [markdown.core :refer [md-to-html-string]]))
 
 (def playground-path (atom ()))
+(def reference-path (atom ()))
 
 (add-filter! :safe (fn [x] [:safe x]))
 
+(defn set-reference-path [env]
+  (swap! reference-path (fn [val & args]
+                          (case env
+                            "docs.anychart.com" "//api.anychart.com/"
+                            "docs.anychart.dev" "//api.anychart.dev/"
+                            "docs.anychart.stg" "//api.anychart.stg/"
+                            "//api.anychart.stg/"))))
+
 (defn set-playground-path [env]
+  (set-reference-path env)
   (swap! playground-path (fn [val & args]
                            (case env
                              "docs.anychart.com" "//playground.anychart.com/acdvf-docs"
@@ -15,20 +25,49 @@
                              "docs.anychart.stg" "//playground.anychart.stg/acdvf-docs"
                              "//playground.anychart.stg/acdvf-docs"))))
 
-(defn build-sample-embed [sample-path]
-  (str
-   "<div class='sample'>
+(defn build-sample-embed [sample-path custom-settings]
+  (let [width (:width custom-settings)
+        height (:height custom-settings)
+        div-style (if (not (= width nil))
+                    (str "style='width:" (+ width 10) "px;'")
+                    "")
+        style (if (and (not (= width nil))
+                       (not (= height nil)))
+                (str "style='width:" width "px;height:" height "px;'")
+                "style='margin-left:50px; margin-right: 50px;'")]
+    (str
+     "<div class='sample' " div-style ">
      <p>Live sample</p>
      <a target='_blank' href='" @playground-path "/{{VERSION}}/samples/" sample-path "-plain'>Launch in playground</a>
-     <iframe src='" @playground-path "/{{VERSION}}/samples/" sample-path "-iframe'></iframe></div>"))
+     <iframe " style "src='" @playground-path "/{{VERSION}}/samples/" sample-path "-iframe'></iframe></div>")))
+
 (defn sample-transformer [text state]
   [(if (or (:code state) (:codeblock state))
      text
-     (let [matches (re-matches #".*(\{sample\}(.*)\{sample\}).*" text)
-           sample-path (nth matches 2)
-           source (nth matches 1)]
+     (let [matches (re-matches #".*(\{sample([^}]*)\}(.*)\{sample\}).*" text)
+           sample-path (last matches)
+           source (nth matches 1)
+           custom-settings (read-string (str "{" (nth matches 2) "}"))]
        (if sample-path
-         (str-utils/replace text source (build-sample-embed sample-path))
+         (str-utils/replace text source (build-sample-embed sample-path custom-settings))
+         text)))
+   state])
+
+(defn build-reference-link [title link]
+  (let [link (if (re-find #"\#.*" link)
+               (clojure.string/replace link #"\#" ".html#")
+               (str link ".html"))]
+    (str "<a href='" @reference-path "{{VERSION}}/" link "'>" title "</a>")))
+
+(defn api-reference-transformer [text state]
+  [(if (or (:code state) (:codeblock state))
+     text
+     (let [matches (re-matches #".*(\{api:([^}]+)\}(.*)\{api\}).*" text)
+           source (nth matches 1)
+           link (nth matches 2)
+           title (last matches)]
+       (if matches
+         (str-utils/replace text source (build-reference-link title link))
          text)))
    state])
 
@@ -36,5 +75,6 @@
   (set-playground-path env)
   (clojure.string/replace (md-to-html-string content
                                              :heading-anchors true
-                                             :custom-transformers [sample-transformer])
+                                             :custom-transformers [sample-transformer
+                                                                   api-reference-transformer])
                           #"\{\{VERSION\}\}" version))
