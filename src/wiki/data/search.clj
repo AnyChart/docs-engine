e0bfd2a4cba71b436f090768b475d708fdcf9933
Alex Batsuev <batsuev@gmail.com>
2015-11-12 16:04:53 +0800

    more experiments

--- src/wiki/data/search.clj
+++ src/wiki/data/search.clj
@@ -4,5 +4,5 @@
 (defn search-for [jdbc query version]
   (let [res (sphinx/query jdbc "select url, snippet(content, ?) as sn from docs_stg_index where match(?) and version_id=? limit 100;" [query query version])]
     (reduce (fn [res row]
-              (str res "<h1>" (get row "url") "</h1>" (get row "sn") "<hr />"))
+              (str res "<h1>" (:url row) "</h1>" (:sn row) "<hr />"))
             "" res)))
