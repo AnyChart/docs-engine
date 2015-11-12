08734a627dd01e6092b8ceaa23e0fcfaede33052
Alex Batsuev <batsuev@gmail.com>
2015-11-12 16:02:16 +0800

    simple formatting

--- src/wiki/data/search.clj
+++ src/wiki/data/search.clj
@@ -2,4 +2,7 @@
   (:require [wiki.components.sphinx :as sphinx]))
 
 (defn search-for [jdbc query version]
-  (sphinx/query jdbc "select url, snippet(content, ?) from docs_stg_index where match(?) and version_id=? limit 100;" [query query version]))
+  (let [res (sphinx/query jdbc "select url, snippet(content, ?) as sn from docs_stg_index where match(?) and version_id=? limit 100;" [query query version])]
+    (reduce (fn [res row]
+              (str res "<h1>" (get row "url") "</h1>" (get row "sn") "<hr />"))
+            "" res)))
