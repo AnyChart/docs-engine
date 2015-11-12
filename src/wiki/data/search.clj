0fb841b703bb81c9b746e57de6561417c9cb4705
Alex Batsuev <batsuev@gmail.com>
2015-11-12 16:18:17 +0800

    :-/

--- src/wiki/data/search.clj
+++ src/wiki/data/search.clj
@@ -2,10 +2,10 @@
   (:require [wiki.components.sphinx :as sphinx]))
 
 (defn search-for [jdbc query version-id version-key]
-  (let [res (sphinx/query jdbc "select url,full_name,snippet(content, ?) as sn from docs_stg_index where match(?) and version_id=? limit 100;" [query query version-id])]
+  (let [res (sphinx/query jdbc "select url,snippet(content, ?) as sn from docs_stg_index where match(?) and version_id=? limit 100;" [query query version-id])]
     (reduce (fn [res row]
               (let [url (str "/" version-key "/" (:url row))]
-                (str res "<h2><a href='" url "'>"
-                     (:full_name row) "</a></h2>"
+                (str res "<strong><a href='" url "'>"
+                     url "</a></strong><br />"
                      (:sn row) "<hr />")))
             "" res)))
