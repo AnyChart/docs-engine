03a95107aa84863fa52313c5a893ea7214c90dbf
Alex Batsuev <batsuev@gmail.com>
2015-11-18 16:54:36 +0800

    remove debug search

--- src/wiki/data/search.clj
+++ src/wiki/data/search.clj
@@ -3,4 +3,4 @@
             [ring.util.response :refer [content-type response]]))
 
 (defn search-for [jdbc query version-id version-key]
-  (sphinx/query jdbc "select url,snippet(content, ?) as sn from docs_stg_index where match(?) and version_id <> ? limit 100;" [query query version-id]))
+  (sphinx/query jdbc "select url,snippet(content, ?) as sn from docs_stg_index where match(?) and version_id = ? limit 100;" [query query version-id]))
