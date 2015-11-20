58a9c3ee37708c3d92ce2854907912d9f2bd83e4
worli4 <vorobyeva.irk@gmail.com>
2015-11-20 13:36:44 +0800

    styles

--- client/js/toc.js
+++ client/js/toc.js
@@ -20,6 +20,7 @@ function fixToc() {
         $("#table-of-content-small").append($next.clone());
         $("#table-of-content-large").append($next.clone());
         $next.remove();
+        $('#article-content ul:first-child').css('display', 'block');
     }
 };
 
