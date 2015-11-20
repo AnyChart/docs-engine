1e893297810e5ba265901e882c27854bc9357fe3
Alex Batsuev <batsuev@gmail.com>
2015-11-20 16:24:09 +0800

    fixes

--- client/js/mobile.js
+++ client/js/mobile.js
@@ -7,3 +7,10 @@ $("#shadow").click(function() {
     $("#bar").toggle();
     $("#shadow").toggle();
 });
+
+$(window).keyup(function(e) {
+    if (e.keyCode == 27) {
+        $("#bar").hide();
+        $("#shadow").hide();
+    }
+});
