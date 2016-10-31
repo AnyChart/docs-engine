a6ee848a0560450d7203b52613326fe49c27c4eb
ilevd <ilevd@mail.ru>
2016-10-31 13:07:56 +0700

    Merge: add anychart-ui.min.css

--- src/wiki/util/utils.clj
+++ src/wiki/util/utils.clj
@@ -7,3 +7,8 @@
   (if (released-version? version-key)
     (str "https://cdn.anychart.com/js/" version-key "/anychart-bundle.min.js")
     (str "http://static.anychart.com/js/" version-key "/anychart-bundle.min.js")))
+
+(defn anychart-bundle-css-url [version-key]
+  (if (released-version? version-key)
+    (str "https://cdn.anychart.com/css/" version-key "/anychart-ui.min.css")
+    (str "http://static.anychart.com/css/" version-key "/anychart-ui.min.css")))
