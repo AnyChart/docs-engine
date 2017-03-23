153b3292ca7cf44cb05b106ecb63b0f64b963308
ilevd <ilevd@mail.ru>
2017-03-23 10:48:20 +0700

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,33 +1,33 @@
 (defproject wiki "2.0"
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [version-clj "0.1.2"]
                  ;; general
-                 [com.stuartsierra/component "0.2.3"]
-                 [clj-time "0.11.0"]
-                 [com.climate/claypoole "1.1.3"]
+                 [com.stuartsierra/component "0.3.2"]
+                 [clj-time "0.13.0"]
+                 [com.climate/claypoole "1.1.4"]
                  ;; templates
                  [selmer "1.10.6"]
                  ;; html
                  [enlive "1.1.6"]
-                 [org.apache.commons/commons-lang3 "3.4"]
+                 [org.apache.commons/commons-lang3 "3.5"]
                  [com.anychart/playground-samples-parser "0.1.3"]
                  [org.jsoup/jsoup "1.10.2"]
                  ;; web
-                 [http-kit "2.1.16"]
-                 [compojure "1.1.9"]
-                 [ring/ring-json "0.3.1"]
-                 [ring/ring-core "1.3.1"]
+                 [http-kit "2.2.0"]
+                 [compojure "1.5.2"]
+                 [ring/ring-json "0.4.0"]
+                 [ring/ring-core "1.5.1"]
                  ;; logging
-                 [com.taoensso/timbre "4.3.1"]
+                 [com.taoensso/timbre "4.8.0"]
                  ;; databases
-                 [com.taoensso/carmine "2.11.1"]
-                 [org.clojure/java.jdbc "0.3.6"]
-                 [postgresql/postgresql "8.4-702.jdbc4"]
-                 [mysql/mysql-connector-java "5.1.36"]
+                 [com.taoensso/carmine "2.15.1"]
+                 [org.clojure/java.jdbc "0.6.1"]
+                 [postgresql/postgresql "9.3-1102.jdbc41"]
+                 [mysql/mysql-connector-java "6.0.6"]
                  [clojure.jdbc/clojure.jdbc-c3p0 "0.3.2"]
-                 [honeysql "0.6.3"]
+                 [honeysql "0.8.2"]
                  ;; markdown
-                 [markdown-clj "0.9.89"]
+                 [markdown-clj "0.9.98"]
                  ;; filesystem
                  [me.raynes/fs "1.4.6"]
                  [cpath-clj "0.1.2"]
