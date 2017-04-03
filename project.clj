84e69f7ab776071364e2237263208854487c624b
ilevd <ilevd@mail.ru>
2017-04-03 12:42:20 +0700

    Add kibit plugin

--- project.clj
+++ project.clj
@@ -38,7 +38,8 @@
                  [criterium "0.4.4"]
                  ;; optimizations
                  [com.googlecode.htmlcompressor/htmlcompressor "1.5.2"]]
-  :plugins [[lein-ancient "0.6.10"]]
+  :plugins [[lein-ancient "0.6.10"]
+            [lein-kibit "0.1.3"]]
   :main wiki.core
   :target-path "target/%s"
   :aot :all
