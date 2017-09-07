b3b5d3921ac5af8c0962f140f0a4e8c70e80172f
ilevd <ilevd@mail.ru>
2017-09-07 14:59:42 +0700

    Redesign: tablet menu showing

--- client-new/js/mobile.js
+++ client-new/js/mobile.js
@@ -10,17 +10,27 @@ function toggleMobileMenu(){
 }
 
 function showMobileMenu(){
+    showPage = false;
     $(".left-sidebar-container").show();
     $("#page-content").hide();
     $(".ac.ac-bars").hide();
     $(".ac.ac-remove").show();
+    // make nice view under white shadow
+    $("#page-content").addClass("show-tablet-menu");
+    $(".right-bar-side").addClass("show-tablet-menu");
+    $(".mobile-search-container").addClass("show-tablet-menu");
 }
 
 function hideMobileMenu(){
+    showPage = true;
     $(".left-sidebar-container").hide();
     $("#page-content").show();
     $(".ac.ac-bars").show();
     $(".ac.ac-remove").hide();
+    // make nice view under white shadow - return back
+    $("#page-content").removeClass("show-tablet-menu");
+    $(".right-bar-side").removeClass("show-tablet-menu");
+    $(".mobile-search-container").removeClass("show-tablet-menu");
 }
 
 $(".sidebar-switcher").click(function() {
