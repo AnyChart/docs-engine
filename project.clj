f7a12cd04d56032b364535ca4c8e4024fc5be449
ilevd <ilevd@mail.ru>
2017-11-07 14:38:06 +0700

    Update link-checker

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                  [enlive "1.1.6"]
                  [org.apache.commons/commons-lang3 "3.5"]
                  [com.anychart/playground-samples-parser "0.1.3"]
-                 [com.anychart/link-checker "0.1.9"]
+                 [com.anychart/link-checker "0.2.0"]
                  [org.jsoup/jsoup "1.10.2"]
                  ;; web
                  [http-kit "2.2.0"]
