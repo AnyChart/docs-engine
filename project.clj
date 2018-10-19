0a8c66f4c8d7c9520b1ebcdd81b2bef1f0731651
ilevd <ilevd@mail.ru>
2018-10-19 14:25:24 +0700

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,16 +3,16 @@
                  [version-clj "0.1.2"]
                  ;; general
                  [com.stuartsierra/component "0.3.2"]
-                 [clj-time "0.14.4"]
+                 [clj-time "0.15.0"]
                  [com.climate/claypoole "1.1.4"]
                  [toml "0.1.3"]
                  ;; templates
-                 [selmer "1.12.1"]
+                 [selmer "1.12.2"]
                  [hiccup "1.0.5"]
                  ;; html
                  [enlive "1.1.6"]
                  [org.apache.commons/commons-lang3 "3.8.1"]
-                 [com.anychart/playground-samples-parser "0.2.5"]
+                 [com.anychart/playground-samples-parser "0.2.6"]
                  [com.anychart/link-checker "0.2.10"]
                  [org.jsoup/jsoup "1.11.3"]
                  [garden "1.3.6"]
@@ -25,7 +25,7 @@
                  ;; logging
                  [com.taoensso/timbre "4.10.0"]
                  ;; databases
-                 [com.taoensso/carmine "2.19.0"]
+                 [com.taoensso/carmine "2.19.1"]
                  ;; there are some problems with new jdbc using sphinx
                  [org.clojure/java.jdbc "0.7.8"]
                  [mysql/mysql-connector-java "5.1.36"]
@@ -33,7 +33,7 @@
                  [clojure.jdbc/clojure.jdbc-c3p0 "0.3.3"]
                  [honeysql "0.9.4"]
                  ;; markdown
-                 [markdown-clj "1.0.3"]
+                 [markdown-clj "1.0.4"]
                  ;; filesystem
                  [me.raynes/fs "1.4.6"]
                  [cpath-clj "0.1.2"]
